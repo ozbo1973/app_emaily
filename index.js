dba33e3515581dc71a5b5f4f6ff6092c8da9a945
ozbo1973 <bbbove20@gmail.com>
2017-11-20 11:01:26 -0700

    Finishing Auth flow.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/Users'); //users collections
+require('./services/passport'); //no assignments needed for passport
+
+mongoose.connect(keys.mongooseURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ yo: 'Wussup' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieCode]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log(`running on port: ${PORT}`);
---
+++ models/Users.js
@@ -0,0 +1,9 @@
+//Users Collection
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
